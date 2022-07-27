30281bfb4bb561b35d590f888c1328ce4bf06678
Xiti922 <100534270+Xiti922@users.noreply.github.com>
2022-07-27 13:32:45 +0200

    Update and rename config.var.example.ts to config.var.ts

---
+++ packages/extension/src/config.var.ts
@@ -0,0 +1,8 @@
+import { AxiosRequestConfig } from "axios";
+
+export const COSMOS_RPC_ENDPOINT = "https://rpc-chronic.zenchainlabs.io/";
+export const COSMOS_RPC_CONFIG: AxiosRequestConfig | undefined = undefined;
+export const COSMOS_REST_ENDPOINT = "https://api-chronic.zenchainlabs.io/";
+export const COSMOS_REST_CONFIG: AxiosRequestConfig | undefined = undefined;
+
+export const PRIVILEGED_ORIGINS: string[] = [];
